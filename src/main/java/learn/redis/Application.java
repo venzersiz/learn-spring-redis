797d6514f4443288bc9ae822b757c568e33d4790
Krevis <venzersiz@gmail.com>
2023-11-15 07:59:31 +0900

    학습 테스트 준비

--- src/main/java/learn/redis/Application.java
+++ src/main/java/learn/redis/Application.java
@@ -1,12 +1,26 @@
 package learn.redis;
 
+import jakarta.annotation.PostConstruct;
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableCaching
+@RequiredArgsConstructor
 public class Application {
+
+    private final ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }

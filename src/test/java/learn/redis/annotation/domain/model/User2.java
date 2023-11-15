a5a01ddafaa909a6fe90d6419d0fa9f95be4da61
Krevis <venzersiz@gmail.com>
2023-11-15 13:57:54 +0900

    GenericJackson2JsonRedisSerializer with different type but same structure

---
+++ src/test/java/learn/redis/annotation/domain/model/User2.java
@@ -0,0 +1,23 @@
+package learn.redis.annotation.domain.model;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Getter
+public class User2 {
+
+    private Long seq;
+
+    private String name;
+
+    private List<Address> addresses;
+
+    public User2(Long seq, String name, List<Address> addresses) {
+        this.seq = seq;
+        this.name = name;
+        this.addresses = addresses;
+    }
+}
--- src/test/java/learn/redis/cache/RedisCacheTest.java
+++ src/test/java/learn/redis/cache/RedisCacheTest.java
@@ -1,10 +1,12 @@
 package learn.redis.cache;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import learn.redis.annotation.domain.model.Address;
 import learn.redis.annotation.domain.model.User;
+import learn.redis.annotation.domain.model.User2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -93,5 +95,22 @@ class RedisCacheTest {
         assertThat(cachedUser.getName()).isEqualTo("김백세");
         assertThat(cachedUser.getAddresses().get(0).getName()).isEqualTo("주소1");
         assertThat(cachedUser.getAddresses().get(1).getName()).isEqualTo("주소2");
+    }
+
+    @Test
+    void genericJackson2JsonRedisSerializerWithDifferentType() {
+        Cache cache = cacheManager.getCache("e");
+
+        List<Address> addresses = List.of(new Address("주소1"), new Address("주소2"));
+        User user = new User(1L, "김백세", addresses);
+
+        cache.put("user1", user);
+
+        // 다른 애플리케이션에서 캐쉬 조회를 한다고 가정
+        assertThatThrownBy(() -> {
+            User2 cachedUser = (User2) cache.get("user1").get();
+        }).isInstanceOf(ClassCastException.class);
+        // 동일 구조의 모델임에도 불구하고 클래스명이 달라서 예외가 발생한다
+        // 클래스명이 동일하고 패키지가 달라도 예외 발생
     }
 }

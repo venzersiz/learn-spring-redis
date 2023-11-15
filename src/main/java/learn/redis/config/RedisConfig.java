21b5b66a8bfcac2655a26f895e7191bc5eb77882
Krevis <venzersiz@gmail.com>
2023-11-15 09:19:15 +0900

    RedisCacheManager, RedisCache

---
+++ src/main/java/learn/redis/config/RedisConfig.java
@@ -0,0 +1,10 @@
+package learn.redis.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+}
